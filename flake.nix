d23c74b3f962b9a743176451948a3365201b747b
Diamondy4 <diamondy4@hotmail.com>
2023-06-19 12:56:23 +0300

    Flake update

--- flake.nix
+++ flake.nix
@@ -5,31 +5,30 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
     clickhouse-driver = {
       url = "github:GetShopTV/clickhouse-driver";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         inputs.haskell-flake.flakeModule
       ];
-      perSystem = { self', inputs', system, config, final, lib, pkgs, ... }:
-        {
-          haskellProjects.default = {
-            imports = [
-              inputs.clickhouse-driver.haskellFlakeProjectModules.output
-            ];
-            basePackages = pkgs.haskell.packages.ghc926;
-          };
-          packages.default = config.packages.persistent-clickhouse;
+      perSystem = {self', ...}: {
+        haskellProjects.default = {
+          imports = [
+            inputs.clickhouse-driver.haskellFlakeProjectModules.output
+          ];
         };
+        packages.default = self'.packages.persistent-clickhouse;
+      };
     };
 }
